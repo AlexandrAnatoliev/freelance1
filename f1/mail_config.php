82488f7f5cc8896f4378984e535086289ad935f0
AlexandrAnatoliev <per-1986@list.ru>
2026-04-19 21:33:47 +0300

    fix: add mail_config

--- f1/mail_config.php
+++ f1/mail_config.php
@@ -3,7 +3,7 @@ use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
-require 'vendor/autoload.php';
+require_once 'vendor/autoload.php';
 
 // загрузка .env файла
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
