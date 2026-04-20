36a1a8ed4ab8ed6ae41e414003f26074662e6ed2
AlexandrAnatoliev <per-1986@list.ru>
2026-04-20 18:42:05 +0300

    fix: remove useless code

--- f1/mail_config.php
+++ f1/mail_config.php
@@ -4,22 +4,17 @@ use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
 require_once 'vendor/autoload.php';
-
-// загрузка .env файла
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-$dotenv->load();
+require_once 'configs/mail.php';
 
 function sendInvoiceEmail($toEmail, $toName, $subject, $htmlBody) {
   $mail = new PHPMailer(true);
 
-  $username = $_ENV['MAIL_USERNAME'] ?? getenv('MAIL_USERNAME');
-  $password = $_ENV['MAIL_PASSWORD'] ?? getenv('MAIL_PASSWORD');
-  $host = $_ENV['MAIL_HOST'] ?? 'smtp.gmail.com';
-  $port = $_ENV['MAIL_PORT'] ?? 587;
-  $encryption = $_ENV['MAIL_ENCRYPTION'] === 'ssl'
-    ? PHPMailer::ENCRYPTION_SMTPS
-    : PHPMailer::ENCRYPTION_STARTTLS;
-  $charset = $_ENV['MAIL_CHARSET'] ?? 'UTF-8';
+  global $username;
+  global $password;
+  global $host;
+  global $port;
+  global $encryption;
+  global $charset;
 
   try {
     // для вывода диагностики в браузер включить DEBUG_SERVER
