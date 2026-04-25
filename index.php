3b42a0d6714d5480d4e06338bdd89ae47aa51bcb
AlexandrAnatoliev <per-1986@list.ru>
2026-04-25 11:38:02 +0300

    fix(index): add types

--- index.php
+++ index.php
@@ -22,7 +22,7 @@ $addons = [
 $_SESSION['items_session'] = $items;
 $_SESSION['addons_session'] = $addons;
 
-function getImagePath($path, $placeholder = 'img/placeholder.jpg')
+function getImagePath(string $path, string $placeholder = 'img/placeholder.jpg'): string
 {
     if (!empty($path) && file_exists($path)) {
         return $path;
