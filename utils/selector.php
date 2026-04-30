1a359a32ab78e6ac99121d8d4bce425fb3a3c398
AlexandrAnatoliev <per-1986@list.ru>
2026-04-30 12:45:13 +0300

    fix(utils): update selector

--- utils/selector.php
+++ utils/selector.php
@@ -2,17 +2,17 @@
 
 function getSelector(): string
 {
-    $option = '
-      <option value="50">50 шт.</option>
-      <option value="100">100 шт.</option>
-      <option value="150">150 шт.</option>
-      <option value="200">200 шт.</option>
-      <option value="250">250 шт.</option>
-      <option value="300">300 шт.</option>
-      <option value="350">350 шт.</option>
-      <option value="400">400 шт.</option>
-      <option value="450">450 шт.</option>
-      <option value="500">500 шт.</option>';
+    $min = 50;
+    $max = 1000;
+    $step = 50;
+    $value = $min;
 
+    $option = '';
+
+    while ($value <= $max) {
+        $option .= '
+          <option value="' . $value . '">' . $value . ' шт.</option>';
+        $value += $step;
+    }
     return $option;
 }
